8167a812706da8a65815d5ec56bcc0a84ba24cb7
Илья Соколов <sokolovila543@gmail.com>
2023-04-24 18:27:10 +0300

    12

--- about_tables.py
+++ about_tables.py
@@ -3,32 +3,38 @@ table_info ={
 
     'students' : {
         'columns': ['id_student','fio','login','pswd', 'email'],
+        'columns_rus': ["Номер студента","ФИО",'Логин',"Пароль","Почта"],
         'fkey': ['progress']
     },
     'manager': {
         'columns': ['id_manager','full_name','login','pswd','email','id_area'],
+        'columns_rus': ["Номер менеджера", "ФИО",'Логин',"Пароль","Почта","Номер предм.области"],
         'fkey': []
     },
     'teachers':
     {
         'columns': ['id_teacher','fio','login','pswd', 'email']
         ,
+        'columns_rus': ["Номер преподавателя","ФИО",'Логин',"Пароль","Почта"],
         'fkey':['courses']
     },
     'courses':
     {
         'columns': ['id_course','course_name', 'price','duration','id_area','id_teacher'],
+        'columns_rus': ["Номер курса","Название курса","Цена","Длительность","Номер предм.области","Номер преподавателя"],
         'fkey': ['progress']
     },
     'subject_area':
     {
         'columns': ['id_area','area_name']
         ,
+        'columns_rus': ["Номер области","Название"],
         'fkey': ['courses', 'manager']
     },
     'progress':
     {
         'columns': ['id_progress','id_student','id_course','is_complete','score'],
+        'columns_rus': ["Номер записи", "Номер студента","Номер курса","Выполенение","Оценка"],
         'fkey': []
     }
 
